35571bbf734480f18b74a83fba11310470f38ff8
Gyusun Yeom <omniavinco@gmail.com>
2021-01-28 21:12:34 +0900

    Fill windows

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const Map = require('./map').default;
 
 // TODO: fill window
 const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
     backgroundColor: 0x1099bb,
     resolution: window.devicePixelRatio || 1,
 });
 document.body.appendChild(app.view);
+window.addEventListener('resize', (e) => {
+  app.resizeTo = window;
+});
 
 const container = new PIXI.Container();
 
