4544a6c152f287c2485b4fdc3c646ecf389055e8
Gyusun Yeom <omniavinco@gmail.com>
2021-01-29 19:17:27 +0900

    parcel integreted server

--- server/main.js
+++ server/main.js
@@ -14,18 +14,17 @@ const pass_options = {
 
 const games = new Map();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   if (!req.query) {
     xkcdp.generate(pass_options, (err, pass) => {
       res.redirect(`/?id=${pass}`);
       res.end();
     });
   } else {
-    res.sendFile(path.join(process.cwd(), 'dist/index.html'));
+    req.url = '/index.html';
+    next('route');
   }
 });
-
-app.use(express.static('./dist'));
 
 const fs = require('fs');
 const map_data = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'map.json')));
@@ -36,5 +35,16 @@ app.ws('/ws/:id', (ws) => {
     map: map_data,
   }));
 });
+
+
+if (process.env.NODE_ENV !== 'production') {
+  const parcel = require('parcel-bundler');
+  const bundler = new parcel(
+    path.join(process.cwd(), 'client/index.html'),
+  );
+  app.use(bundler.middleware());
+} else {
+  app.use(express.static('./dist'));
+}
 
 app.listen(1233);
