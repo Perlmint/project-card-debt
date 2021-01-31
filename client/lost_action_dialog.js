87f4e325779153bbcb3067d83d5052b133e769c3
YangHun <pitahano@gmail.com>
2021-01-31 11:30:18 +0900

    apply lost_action_dialog  ui

--- client/lost_action_dialog.js
+++ client/lost_action_dialog.js
@@ -1,25 +1,46 @@
 const EventEmitter = require('eventemitter3');
-export default class ActionDialog extends PIXI.Container {
+const background = PIXI.Texture.from(require('./res/move_background.png'));
+const action_background = PIXI.Texture.from(require('./res/alarm.png'));
+
+class ActionItem extends PIXI.NineSlicePlane {
   constructor() {
-    super();
+    super(action_background, 32, 32, 32, 32);
+
+    this.width = 266 + 12 * 2;
+    this.height = 87;
+
+    this.title = new PIXI.Text('', {
+      fontSize: 16,
+      fontWeight: 500,
+    });
+    this.title.alpha = 0.87;
+    this.title.position.set(20 + 12, 20 + 7);
+    this.addChild(this.title);
+  }
+
+  init(action) {
+    this.title.text = action.action_name;
+  }
+}
+
+export default class ActionDialog extends PIXI.NineSlicePlane {
+  constructor() {
+    super(background, 32, 32, 32, 32);
+
     /** @memeber */
-    this.frame = new PIXI.Graphics();
-    this.addChild(this.frame);
+    this.width = 330 + 12 * 2;
+
     /** @member */
     this.name_label = new PIXI.Text("", {
-      fontSize: 30,
+      fontWeight: 700,
+      fontSize: 20,
     });
-    this.name_label.position.set(10, 10);
+    this.name_label.position.set(165 + 12, 27 + 7);
+    this.name_label.anchor.x = 0.5;
     this.addChild(this.name_label);
-    /** @member {PIXI.Text[]}*/
-    this.action_labels = [];
-    this.do_button = new PIXI.Graphics();
-    this.do_button.interactive = true;
-    this.do_button.buttonMode = true;
-    this.do_button.on('pointertap', () => {
-      this.parent.removeChild(this.root);
-    })
-    this.addChild(this.do_button);
+
+    /** @member {ActionItem[]}*/
+    this.action_buttons = [];
   }
 
   /**
@@ -34,43 +55,35 @@ export default class ActionDialog extends PIXI.Container {
     let height = 0;
     this.name_label.text = name;
     this.name_label.updateText();
-    height += this.name_label.height + 10;
-    for (const label of this.action_labels) {
+    height += this.name_label.height + 20 + this.name_label.y;
+    for (const label of this.action_buttons) {
       this.removeChild(label);
     }
     let width = this.name_label.width;
     for (let i = 0; i < actions.length; i++) {
       const action = actions[i];
-      let action_label;
-      if (this.action_labels[i]) {
-        action_label = this.action_labels[i];
-        action_label.text = action.action_name;
+      let action_item;
+      if (this.action_buttons[i]) {
+        action_item = this.action_buttons[i];
+        action_item.text = action.action_name;
       } else {
-        action_label = new PIXI.Text(action.action_name);
-        action_label.interactive = true;
-        action_label.buttonMode = true;
-        this.action_labels.push(action_label);
-        const action_id = action.action_id;
-        action_label.on('pointertap', () => {
-          this.emit('do');
-          this.parent.removeChild(this.root);
-        });
+        action_item = new ActionItem();
+        action_item.x = 32;
+        action_item.interactive = true;
+        action_item.buttonMode = true;
+        this.action_buttons.push(action_item);
       }
-      action_label.updateText();
-      action_label.position.set(20, height);
-      this.addChild(action_label);
-      height += this.name_label.height + 10;
-      width = Math.max(action_label.width, width);
+      action_item.on('pointertap', () => {
+        this.emit('do');
+      });
+      action_item.init(action);
+      action_item.y = height;
+      this.addChild(action_item);
+      height += action_item.height;
+      width = Math.max(action_item.width, width);
     }
 
-    this.frame.clear();
-    this.frame.beginFill(0xcc0000, 1);
-    this.frame.drawRoundedRect(0, 0, width + 30, height + 70, 10);
-
-    this.do_button.clear();
-    this.do_button.beginFill(0x003300, 1);
-    this.do_button.drawRoundedRect(10, 10, width + 10, 50, 10);
-    this.do_button.position.set(0, height - 20);
+    this.height = height + 37;
     this.pivot.set(-40, height + 90);
   }
 }
